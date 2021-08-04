ddbdde868d276c620d98dfb1a788e100d54d8114
john batista <batistaj651@gmail.com>
2021-08-03 22:23:44 -0400

    fixed all issues

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,14 +2,6 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// router.get('/', (req, res) => {
-//     Comment.findAll({})
-//       .then(dbCommentData => res.json(dbCommentData))
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-// });
 
 router.post('/', withAuth, (req, res) => {
     console.log(req.body)
@@ -30,23 +22,6 @@ router.post('/', withAuth, (req, res) => {
   }
 });
 
-// router.delete('/:id', withAuth, (req, res) => {
-//     Comment.destroy({
-//         where: {
-//           id: req.params.id
-//         }
-//       })
-//         .then(dbCommentData => {
-//           if (!dbCommentData) {
-//             res.status(404).json({ message: 'No comment found with this id' });
-//             return;
-//           }
-//           res.json(dbCommentData);
-//         })
-//         .catch(err => {
-//           console.log(err);
-//           res.status(500).json(err);
-//         });
-// });
+
 
 module.exports = router;
