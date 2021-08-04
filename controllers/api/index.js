f34e380e5ad8481a94d03b50ccff857b770b091e
john batista <batistaj651@gmail.com>
2021-08-03 22:17:26 -0400

    fix casing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./post-Routes');
+const postRoutes = require('./post-routes');
 const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
